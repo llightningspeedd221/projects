5329ddc5b3479d3e74ed4f48eb9a611c8fd909bb
llightningspeedd221 <lightningspeedd@proton.me>
2025-01-21 21:26:57 +0100

    Update name_esp.lua

--- name_esp.lua
+++ name_esp.lua
@@ -1,6 +1,7 @@
 local players = game.Players
 local localPlayer = game.Players.LocalPlayer
 local ts = game:GetService("TeleportService")
+local uis = game:GetService("UserInputService")
 -- Function to add ESP to a player's character
 local function addESP(player)
 	if player == localPlayer then
@@ -8,6 +9,14 @@ local function addESP(player)
 	end
 	local char = player.Character or player.CharacterAdded:Wait() -- Ensure the character exists
 	local rootPart = char:WaitForChild("HumanoidRootPart") -- Wait for the root part to load
+	
+	-- highlight
+	local highlight = Instance.new("Highlight")
+	highlight.Parent = char
+	highlight.OutlineTransparency = 1
+	highlight.Adornee = char
+	highlight.FillTransparency = 0.5
+	
 
 	-- Create the BillboardGui
 	local nameEsp = Instance.new("BillboardGui")
@@ -23,18 +32,28 @@ local function addESP(player)
 	text.Size = UDim2.new(1, 0, 1, 0) -- Fill the BillboardGui
 	text.BackgroundTransparency = 1 -- Transparent background
 	text.Text = player.Name -- Display the player's name
-	text.TextColor3 = Color3.new(1, 0, 0) -- Red
+	text.TextColor3 = Color3.new(0, 0, 0) -- Red
 	text.TextScaled = true -- Scale text to fit
+	-- stroke
+	local stroke = Instance.new("UIStroke")
+	stroke.Parent = text
+	stroke.Color = Color3.new(1,0,0)
+	stroke.Thickness = 0.4
+	
 end
 
 -- Add ESP for all players currently in the game
 for _, player in pairs(players:GetPlayers()) do
 	addESP(player)
 end
+-- Listen for keypress to teleport the local player back to the same game
+uis.InputBegan:Connect(function(input, gameProcessed)
+	if not gameProcessed and input.KeyCode == Enum.KeyCode.L then
+		ts:Teleport(game.PlaceId, localPlayer)
+	end
+end)
 
 -- Add ESP for players who join the game in the future
 players.PlayerAdded:Connect(function(player)
 	addESP(player)
 end)
-task.wait(4)
-ts:Teleport(game.PlaceId, localPlayer)
